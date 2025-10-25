e78f5a72885cf23838ff84c8a45b9050791009f2
ravisankar666 <ravisankarp666@gmail.com>
2025-10-26 00:54:45 +0530

    some message

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  common_tags = {
+    project = var.project_name
+    environment = var.environment
+    Terraform = true
+  }
+  common_name_suffix = "${var.project_name}-${var.environment}"
+    az_names = slice(data.aws_availability_zone.available,0,2) #used slice function here for selecting 2 things.
+}
